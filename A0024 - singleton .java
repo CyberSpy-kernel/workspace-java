bb9aefa3baa76b80af78875a81b6e252f72f0b8e
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-28 01:32:49 +0530

    Update and rename TP002.java to A0024 - singleton .java

--- A0024 - singleton .java
+++ A0024 - singleton .java
@@ -4,9 +4,9 @@
  * Example 1 of singleton
  */
 
-public class TP002 {
+public class A0024 {
   public static void main(String[] args) {
-    TP001 tmp = TP001.getInstance( ); // Replace TP001 with Singleton
+    A0024 tmp = A0024.getInstance( ); // Replace TP001 with Singleton
     tmp.demoMethod( );
   }
 }
