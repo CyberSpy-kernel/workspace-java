fa2add63a5bc38268d65fea788964190d58e126d
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-28 01:22:21 +0530

    Update and rename P017.java to AB016 - final - variable .java

--- AB016 - final - variable .java
+++ AB016 - final - variable .java
@@ -4,7 +4,7 @@
  * description: this will declare the variable as 'final' or 'constant', which means unchangable and read-only
  */
 
-public class P017 {
+public class AB016 {
   public static void main(String[] args) {
     final int myNum = 123;
     // myNum = 12; // this line will generate an error: cannot assign a value to final variable
