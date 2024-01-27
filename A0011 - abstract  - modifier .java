69d80ccf65443313156aa5bb7f4a0366e55c5faf
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-28 00:40:01 +0530

    Update A0011 - abstract  - modifier .java

--- A0011 - abstract  - modifier .java
+++ A0011 - abstract  - modifier .java
@@ -1,6 +1,8 @@
 /*
  * Abstract Modifier
- * The abstract modifier is used with classes and methods. An abstract class cannot be instantiated, and an abstract method must be implemented by any concrete (non-abstract) subclass.
+ * The abstract modifier is used with classes and methods. 
+ * An abstract class cannot be instantiated, 
+ * and an abstract method must be implemented by any concrete (non-abstract) subclass.
  */
 public class GPT011 {
   
