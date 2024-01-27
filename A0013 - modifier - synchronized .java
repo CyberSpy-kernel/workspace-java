4101991e7e1c74c6187d353515297dcd013e8bac
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-28 00:42:19 +0530

    Update A0013 - modifier - synchronized .java

--- A0013 - modifier - synchronized .java
+++ A0013 - modifier - synchronized .java
@@ -1,6 +1,7 @@
 /*
  * Synchronized modifier
- * the synchronized modifier is used to control access to a method or block of code by multiple threads. it ensures that only one thread can access the synchronized method or block at a time.
+ * the synchronized modifier is used to control access to a method or block of code by multiple threads. 
+ * it ensures that only one thread can access the synchronized method or block at a time.
  */
 public class A0013 {
   private int counter = 0;
