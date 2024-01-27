0220f17ef000464d4ba7798734a6c4af3bad13c5
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-28 01:39:39 +0530

    Update and rename TP007.java to A0027 - singleton - classic .java

--- A0027 - singleton - classic .java
+++ A0027 - singleton - classic .java
@@ -2,14 +2,14 @@
  * Title: classic singleton
  * Example 2 of singleton
  */
-public class TP007 {
-  private static TP007 instance = null;
-  private TP007() {
+public class A0027 {
+  private static A0027 instance = null;
+  private A0027() {
     // EXists only to defeat instantiation
   }
-  public static TP007 getInstance() {
+  public static A0027 getInstance() {
     if(instance == null) {
-      instance = new TP007();
+      instance = new A0027();
     }
     return instance;
   }
