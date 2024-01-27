ea2d7435aa94fe30f7c12b426b86821dfa6b6043
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-28 00:37:21 +0530

    Update and rename GPT009.java to A0009 - access modifier - default .java

--- A0009 - access modifier - default .java
+++ A0009 - access modifier - default .java
@@ -3,7 +3,7 @@
  * if no access modifier is specified, it defaults to package-private, which means the member is accessible within the same package.
  */
 
-public class GPT009 {
+public class A0009 {
   int packagePrivateVariable;
   void packagePrivateMethod() {
     // code here
