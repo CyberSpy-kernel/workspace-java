6dacfaeb869f28d253d722ac19eaa834918cd0f8
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-28 00:49:12 +0530

    Update A0022 - message passing .java

--- A0022 - message passing .java
+++ A0022 - message passing .java
@@ -8,7 +8,7 @@ class Example {
     System.out.println("Received Message: "+ message);
   }
 }
-public class A0020 {
+public class A0022 {
   public static void main(String[] args) {
     Example obj = new Example();
     obj.showMessage("Hello, Java!");
