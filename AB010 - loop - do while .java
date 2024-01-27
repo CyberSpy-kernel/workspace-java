1d58acd7169f0443d3ca853e80b2d6c65ad9df0a
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-28 01:11:38 +0530

    Update and rename P010.java to AB010 - loop - do while .java

--- AB010 - loop - do while .java
+++ AB010 - loop - do while .java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class P010 {
+public class AB010 {
   public static void main(String[] args) {
     // Create a Scanner object for user input
     Scanner sc = new Scanner(System.in);
