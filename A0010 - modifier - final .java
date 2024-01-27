a2b8e19051c8db162209ec735c6824a0cee25955
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-28 00:38:34 +0530

    Update and rename GPT010.java to A0010 - modifier - final .java

--- A0010 - modifier - final .java
+++ A0010 - modifier - final .java
@@ -2,7 +2,7 @@
  * Final moidifier
  * The final modifier is used to indicate that a variable, method, or class cannot be changed or overridden.
  */
-public class GPT010 {
+public class A0010 {
   final int constantValue = 10;
     
   final void display() {
