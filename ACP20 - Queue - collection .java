1158d2ef254bb3e356d57e89f50f3dbc6a00c22d
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-28 20:48:17 +0530

    Create ACP20 - Queue - collection .java

---
+++ ACP20 - Queue - collection .java
@@ -0,0 +1,24 @@
+/*
+ * Queue using Collection framework
+ */
+
+ import java.util.*;
+
+ public class ACP20 {
+  public static void main(String[] args) {
+
+    Queue<Integer> q = new LinkedList<>();
+    // Queue<Integer> q = new ArrayDeque<>();
+
+    q.add(1);
+    q.add(2);
+    q.add(3);
+    q.add(4);
+
+    while (!q.isEmpty()) {
+      System.out.println(q.peek());
+      q.remove();
+    }
+
+  }
+}
