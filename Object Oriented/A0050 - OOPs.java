2d8e98b0096964a09b5371e550475a0f15e8f289
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-04 11:47:21 +0530

    Update and rename ACP05 - OOPs.java to A0050 - OOPs.java

--- Object Oriented/A0050 - OOPs.java
+++ Object Oriented/A0050 - OOPs.java
@@ -27,7 +27,7 @@ class Student {
     System.out.println("Constructor called!");
   }
 }
-public class ACP05 {
+public class A0050 {
   public static void main(String[] args) {
     Pen pen1 = new Pen();
     pen1.color = "blue";
