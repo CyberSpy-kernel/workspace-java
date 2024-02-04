80a1a8062941f15e27b18a32d2f98343a4a7a4fe
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-04 19:12:27 +0530

    Create A0071 - String methods .java

---
+++ Strings/A0071 - String methods .java
@@ -0,0 +1,18 @@
+public class A0070 {
+  public static void main(String[] args) {
+    
+  }
+}
+
+ /*
+ * lastIndexOf()
+ * length()
+ * matches()
+ * offsetByCodePoints()
+ * regionMatches()
+ * replace()
+ * replaceFirst()
+ * replaceAll()
+ * split()
+ * startsWith()
+  */
