5cd06884cab35ec99a7fa6e71c4b1d6c633f6845
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-04 11:35:33 +0530

    Update A0040- loop - do while .java

--- Basics/A0040- loop - do while .java
+++ Basics/A0040- loop - do while .java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class AB009 {
+public class A0040 {
   public static void main(String[] args) {
     // Create a Scanner object for user input
     Scanner sc = new Scanner(System.in);
