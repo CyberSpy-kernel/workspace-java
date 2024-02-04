b6c8c727109b9df824bf5b1d861f8a9b90d1e8fd
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-04 19:10:38 +0530

    Create A0069 - String methods .java

---
+++ Strings/A0069 - String methods .java
@@ -0,0 +1,19 @@
+// String method
+public class A0069 {
+  public static void main(String[] args) {
+    
+  }
+}
+
+/*
+ * endsWith()
+ * equals()
+ * equalsIgnoreCase()
+ * format()
+ * getBytes()
+ * getChars()
+ * hashCode()
+ * indexOf()
+ * intern()
+ * isEmpty()
+ */
