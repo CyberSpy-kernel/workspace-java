03b674a55d187546714ae196f945c7a55a473e28
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-04 19:13:02 +0530

    Update A0071 - String methods .java

--- Strings/A0071 - String methods .java
+++ Strings/A0071 - String methods .java
@@ -1,18 +1,15 @@
-public class A0070 {
-  public static void main(String[] args) {
-    
-  }
+public class A0071 {
+  
 }
 
- /*
- * lastIndexOf()
- * length()
- * matches()
- * offsetByCodePoints()
- * regionMatches()
- * replace()
- * replaceFirst()
- * replaceAll()
- * split()
- * startsWith()
-  */
+
+/*
+ * subSequence()
+ * substring()
+ * toCharArray()
+ * toLowerCase()
+ * toString()
+ * toUpperCase()
+ * trim()
+ * valueOf()
+ */
