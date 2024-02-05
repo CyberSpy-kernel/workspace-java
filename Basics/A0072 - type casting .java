f3b516db3530a2331f5ce923b957d05ead130ac6
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-05 10:14:01 +0530

    Update A0072 - type casting .java

--- Basics/A0072 - type casting .java
+++ Basics/A0072 - type casting .java
@@ -6,13 +6,21 @@
 public class A0072 {
     public static void main(String[] args) {
         int i = 8;
-        double d = i;   // automatic casting
+        long l = i;   // automatic casting / implicit casting from int to long
         
-        System.out.println(i);
-        System.out.println(d);
+        System.out.println("Line 1: "+i);
+        System.out.println("Line 2: "+l);
 
-        i = (int) d;    // manual casting
-        System.out.println(d);
-        System.out.println(i);
+        double d = 10.5;
+        i = (int) d;    // manual casting / explicit casting from double to int
+        System.out.println("Line 3: "+d);
+        System.out.println("Line 4: "+i);
+
+        char c = (char) i; // explicit casting from int to char
+        System.out.println("Line 5: "+c);
+
+        String s = "123";
+        int ia = Integer.parseInt(s);
+        System.out.println("Line 6: "+ia);
     }
 }
