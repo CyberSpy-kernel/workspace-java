526d196cae5b64a19e00f52b7d6711d82a34e747
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-06 09:38:21 +0530

    Update A0075 - Binary search tree .java

--- Data Structures/A0075 - Binary search tree .java
+++ Data Structures/A0075 - Binary search tree .java
@@ -38,6 +38,75 @@ public class A0074 {
     System.out.print(root.data+" ");
     inorder(root.right);
   }
+
+  public static boolean search(Node root, int key) {
+    if (root == null) {
+      return false;
+    }
+    if(root.data > key) { // left subtree
+      return search(root.left, key);
+    }
+    else if(root.data == key) {
+      return true;
+    }
+    else {
+      return search(root.right, key);
+    }
+  }
+
+  public static Node delete(Node root, int val) {
+    if(root.data > val) {
+      root.left = delete(root.left, val);
+    }
+    else if( root.data < val ) {
+      root.right = delete(root.right, val);
+    }
+    else {
+      // root.data == val
+      // case 1
+      if(root.left == null && root.right == null) {
+        return null;
+      }
+
+      // case 2 
+      if(root.left == null) {
+        return root.right;
+      }
+      else if(root.right == null) {
+        return root.left;
+      }
+
+      // case 3 
+      Node IS = inorderSuccessor(root.right);
+      root.data = IS.data;
+      root.right = delete(root.right, IS.data);
+
+    }
+    return root;
+  }
+
+  public static Node inorderSuccessor(Node root) {
+    while (root.left != null) {
+      root = root.left;
+    }
+
+    return root;
+  }
+
+  public static void printInRange(Node root, int x, int y) {
+    if(root == null) {
+      return;
+    }
+    if(root.data >= x && root.data <= y) {
+      printInRange(root.left, x, y);
+      System.out.print(root.data+" ");
+      printInRange(root.right, x, y);
+    } else if(root.data >= y) {
+      printInRange(root.left, x, y);
+    } else {
+      printInRange(root.right, x, y);
+    }
+  }
   public static void main(String[] args) {
     int values[] = {5, 1, 3, 4, 2, 7};
     Node root = null;
@@ -47,7 +116,16 @@ public class A0074 {
     }
     inorder(root);
     System.out.println();
-  }
 
-  
+    // if(search(root, 7)) {
+    //   System.out.println("found");
+    // } else {
+    //   System.out.println("not found");
+    // }
+
+    // delete(root, 3);
+    // inorder(root);
+
+    printInRange(root, 3, 10);
+  }
 }
