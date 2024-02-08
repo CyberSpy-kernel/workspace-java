7b2257894fbfc59506c964fc04a2e200e11293d6
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-08 16:27:12 +0530

    Update HR0016 - Java Exception Handling .java

--- HackerRank/HR0016 - Java Exception Handling .java
+++ HackerRank/HR0016 - Java Exception Handling .java
@@ -6,7 +6,7 @@ class MyCalculator {
     long power(int n, int p) throws Exception {
         
         if(n<0 || p<0) throw new Exception("n or p should not be negative.");
-        if(n==0 || p==0) throw new Exception("n and p should not be zero.");
+        if(n==0 && p==0) throw new Exception("n and p should not be zero.");
         long res = 1;
         for (int i = 0; i < p; i++) {
             res *= n;
