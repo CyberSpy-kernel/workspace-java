ab7b0b5a56b63e040843550bb4f912b9d00dcd55
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-10 21:55:36 +0530

    Create A0076 - Generics .java

---
+++ Arrays/Advanced/A0076 - Generics .java
@@ -0,0 +1,21 @@
+package com.java.advanced;
+
+import java.util.*;
+
+public class A0076 {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Artist");
+		list.add("Ghost");
+		
+		// list.add(234); // compile time error
+		
+		String s = list.get(1); // type casting is not required
+		System.out.println("Element is: "+s);
+		
+		Iterator<String> itr=list.iterator();  
+		while(itr.hasNext()){  
+			System.out.println(itr.next());
+		}
+	}
+}
