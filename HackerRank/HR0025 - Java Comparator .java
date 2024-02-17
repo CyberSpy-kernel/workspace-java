33ebef2187fc17f1a645b804947b462e58b503fb
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-17 15:20:52 +0530

    Create HR0025 - Java Comparator .java

---
+++ HackerRank/HR0025 - Java Comparator .java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+// Write your Checker class here
+class Checker implements Comparator<Player>{
+    public int compare(Player i, Player j){
+        if(i.score < j.score){
+            return 1;
+        }else if(i.score == j.score){
+            if (i.name.compareTo(j.name)>=1) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }else{
+            return -1;
+        }
+    }        
+}
+
+
+class Player{
+    String name;
+    int score;
+    
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
