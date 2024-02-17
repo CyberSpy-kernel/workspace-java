1fb927deb5118f52d329a187df4b406b19977d8f
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-02-17 17:45:38 +0530

    Create HR0036 - Java MD5 Discussions .java

---
+++ HackerRank/HR0036 - Java MD5 Discussions .java
@@ -0,0 +1,30 @@
+import java.util.*; 
+import java.math.BigInteger; 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        sc.close();
+        String output = "";
+        try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(input.getBytes());
+            BigInteger bigInteger = new BigInteger(1,digest);
+            
+            String hash = bigInteger.toString(16);
+            while(hash.length() < 32){
+                hash += "0" + hash;
+            }
+
+            output = hash;
+            System.out.println(output);
+        }
+        catch(NoSuchAlgorithmException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
