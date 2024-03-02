f6e1cbf28529dd2d6c3a5da35ac693edec29c635
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-03-02 12:35:25 +0530

    Update A0031 - Bubble sort.java

--- Algorithms/A0031 - Bubble sort.java
+++ Algorithms/A0031 - Bubble sort.java
@@ -17,8 +17,8 @@
     // time complexity = O(n^2)
     // bubble sort
 
-    for(int i =0;i<arr.length-1;i++){
-      for(int j=0;j<arr.length-1-i;j++){
+    for(int i = 0; i < arr.length - 1; i++){
+      for(int j = 0; j < arr.length - 1 - i; j++){
         if(arr[j]>arr[j+1]){
           //swap
           int temp = arr[j];
