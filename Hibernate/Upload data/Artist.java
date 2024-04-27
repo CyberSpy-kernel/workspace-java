b545fbfdf0bd83c342a6243a2a67b23981affdef
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-04-27 20:04:50 +0530

    Create Artist.java

---
+++ Hibernate/Upload data/Artist.java
@@ -0,0 +1,39 @@
+// src\main\java\com\telusko\DemoHib\
+
+package com.artist.DemoHib;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+//@Entity(name="artist_table") // use table name = 'artist_table'
+@Entity
+//@Table(name="alien_table")
+public class Artist {
+	@Id
+	private int aid;
+	@Transient
+	private String aname;
+//	@Column(name="alien_color")
+	private String color;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
