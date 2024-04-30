1566516f0b8ee5c52d0cb9d7621fa1005f93d4cf
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-04-30 16:15:44 +0530

    Create Artist.java

---
+++ Hibernate/Fetch Data - Lazy/Artist.java
@@ -0,0 +1,45 @@
+package com.artist.DemoHib;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Artist {
+	@Id
+	private int aid;
+	private String aname;
+	
+	@OneToMany(mappedBy="artist",fetch=FetchType.EAGER)
+	private Collection<Laptop> laps = new ArrayList<Laptop>();
+
+	public int getAid() {
+		return aid;
+	}
+
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+
+	public String getAname() {
+		return aname;
+	}
+
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+
+	public Collection<Laptop> getLaps() {
+		return laps;
+	}
+
+	public void setLaps(Collection<Laptop> laps) {
+		this.laps = laps;
+	}
+	
+	
+}
